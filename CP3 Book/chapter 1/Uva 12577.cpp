d8f34bf86c6682db0550e3f13a437d391f35fd76
NSDZ <nassim.sbaihia@gmail.com>
2017-07-30 12:50:04 +0200

    Create Uva 12577.cpp

---
+++ CP3 Book/chapter 1/Uva 12577.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Uva 12577.cpp
+// Author      : @Nassim_ sbaihia
+// Copyright   :
+// Description : Uva 12577 solution
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   char str[128];
+   while(scanf("%s", str) != '*'){
+   	printf("Hajj-e-");
+   	if(str[0]=='H') printf("Akbar\n");
+   	else printf("Asghar\n");
+   }
+
+   return(0);
+}
